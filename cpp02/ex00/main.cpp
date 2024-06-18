ce94cabd53e99dee2413ef828b18b64a60e7915b
Arthur Pages <arpages@c2r5p10.42perpignan.fr>
2024-06-18 14:49:33 +0200

    updqte

--- cpp02/ex00/main.cpp
+++ cpp02/ex00/main.cpp
@@ -6,7 +6,21 @@
 /*   By: arpages <arpages@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 14:48:55 by arpages           #+#    #+#             */
-/*   Updated: 2024/06/17 14:48:56 by arpages          ###   ########.fr       */
+/*   Updated: 2024/06/18 13:42:12 by arpages          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+#include <iostream>
+int main( void )
+{
+    Fixed a;
+    Fixed b( a );
+    Fixed c;
+    c = b;
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+    return 0;
+}
